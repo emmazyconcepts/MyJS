bfd6b2197b2ae7a368a25786835e26e6453158e1
ekeemmanuel <emmazy100@gmail.com>
2022-06-04 09:38:12 +0100

    first

---
+++ home.js
@@ -0,0 +1,17 @@
+console.log("Hello World");
+
+//varaible
+//var age = prompt("How old are you?");
+//document.getElementById("test").innerHTML = age;
+
+//number
+var num1 = 10;
+num1++; //increses by 1 cause of plus sign
+num1--; //decreases by 1 cause of minus sign
+num1 = num1 + 10;
+//console.log(50 / 2);
+console.log(num1);
+
+// we can divide, multiply, modulus, exponent
+
+console.log(num1 % 6); //modulus
